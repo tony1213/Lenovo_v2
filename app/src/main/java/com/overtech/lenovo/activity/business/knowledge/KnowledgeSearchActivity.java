604722c7afda4bd83b2d747e9ac77685b2e175fc
Tony1213 <1973265001@qq.com>
2016-04-14 09:03:41 +0800

    知识页面搜索

--- app/src/main/java/com/overtech/lenovo/activity/business/knowledge/KnowledgeSearchActivity.java
+++ app/src/main/java/com/overtech/lenovo/activity/business/knowledge/KnowledgeSearchActivity.java
@@ -1,11 +1,21 @@
 package com.overtech.lenovo.activity.business.knowledge;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 import com.overtech.lenovo.R;
 import com.overtech.lenovo.activity.base.BaseActivity;
+import com.overtech.lenovo.widget.EditTextWithDelete;
 
 public class KnowledgeSearchActivity extends BaseActivity {
+    private EditTextWithDelete mEditTextWithDeleteSearch;
+    private ImageView mDoBack;
+    private GridView mGridViewHot;
+    private ListView mAutoListView;
+
     @Override
     protected int getLayoutIds() {
         return R.layout.activity_knowledge_search;
@@ -13,6 +23,23 @@ public class KnowledgeSearchActivity extends BaseActivity {
 
     @Override
     protected void afterCreate(Bundle savedInstanceState) {
+        findViewById();
+        initEvent();
+
+    }
+    private void findViewById(){
+        mDoBack=(ImageView)findViewById(R.id.imageView_knowledge_back);
+        mGridViewHot=(GridView)findViewById(R.id.gridView_knowledge_search_hot);
+        mEditTextWithDeleteSearch=(EditTextWithDelete)findViewById(R.id.et_knowledge_search);
+        mAutoListView=(ListView)findViewById(R.id.lv_knowledge_search_hot);
+    }
+    private void initEvent(){
+        mDoBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
     }
 }
